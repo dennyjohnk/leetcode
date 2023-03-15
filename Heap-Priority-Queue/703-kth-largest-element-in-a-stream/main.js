afe125354d68b8d38819e0e5e31a3cb37a3997ea
Denny John <dennyjohn@Dennys-MacBook-Pro.local>
2023-03-15 14:01:02 +0530

    first

---
+++ Heap-Priority-Queue/703-kth-largest-element-in-a-stream/main.js
@@ -0,0 +1,10 @@
+const {
+    MinPriorityQueue,
+} = require('@datastructures-js/priority-queue');
+
+class KthLargest {
+    constructor(k, nums) {
+        this.k = k;
+        this.minHeap = new MinPriorityQueue
+    }
+}
